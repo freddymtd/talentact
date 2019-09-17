ffd909221b91811b14f31a7245896cc488676e0d
juliamonaco <50450544+juliamonaco@users.noreply.github.com>
2019-09-17 18:45:26 -0300

    Add files via upload

---
+++ SITE_BAYER.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    
+    <meta charset="utf-8">
+    
+    <title>
+    
+        Site da Bayer
+    </title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        
+    
+    <style type="text/css">
+    
+        #teste{
+            
+            background: #3399ff;
+            
+            height: 80px;
+            color: navajowhite;
+            
+            
+            
+        }
+  
+    
+        
+        #animacao{
+            
+            
+            padding: 0;
+            margin: 0;
+            font-family: verdana;
+           
+            
+        }
+        
+        body{
+            
+            
+            background: #000;
+        }
+        .container span{
+            
+            
+            text-transform: uppercase;
+            display: block;
+        }
+        .container{
+            
+            
+            text-align: center;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            width: 100%;
+            
+                
+        }
+        
+        .text1{
+            
+            color: aqua;
+            font-size: 50px;
+            font-weight: 700;
+            letter-spacing: 8px;
+            margin-bottom: 20px;
+            position: relative;
+            animation: text 3s 1; 
+            font-family: monospace;
+            
+            
+        }
+        
+        .text2{
+            
+            
+            font-size: 45px;
+            color: whitesmoke;
+            font-family:fantasy; 
+            
+            
+        }
+        @keyframes text{
+            
+            0%{
+                
+                color: black;
+                margin-bottom: -40px;
+                text-align: center;
+                
+            }
+            
+            30%{
+                letter-spacing: 25px;
+                margin-bottom: -40px;
+                
+                
+                
+            }
+            85%{
+                
+                letter-spacing: 8px;
+                margin-bottom: -40px;
+                
+            }
+        }
+    </style>
+    
+    
+    
+    
+    </head>
+    
+    <body>
+
+   <div class="container-fluid" id="teste" > 
+        <span class="text1">
+       Cadastro de 
+       </span>
+       <span class="text2">
+       Eventos 
+       </span>
+       
+        
+        </div>
+        
+    
+    </body>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+</html>
